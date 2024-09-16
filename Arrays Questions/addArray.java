93bec3344f38444ba09a523fc68ca3c442ed1816
Naaru2003 <narayandhungana333@gmail.com>
2024-09-16 10:59:55 +0530

    adding arrays

--- Arrays Questions/addArray.java
+++ Arrays Questions/addArray.java
@@ -1,5 +1,6 @@
 // Adding two arrays in one
-
+import java.util.*;
+//////////////////////using loop///////////////////////////////
 public class addArray {
     public static void main(String[] args) {
         
@@ -29,5 +30,31 @@ for(int i = 0;i<arr3.length;i++)
     System.out.print(" "+arr3[i]);
 }
 
+System.out.println();
+////////////////// using arraycopy method////////////////////////////////
+
+int arr4[] = new int[arr1.length+arr2.length];
+
+// copying from 1 to 4
+
+System.arraycopy(arr1,0,arr4,0,arr1.length);
+
+
+// copying from 2 to 4
+
+System.arraycopy(arr2,0,arr4,arr1.length,arr2.length);
+
+
+//printing using second method
+for(int i = 0;i<arr4.length;i++)
+{
+    System.out.print(" "+arr4[i]);
+}
+
+
+
+
+
     }
 }
+
